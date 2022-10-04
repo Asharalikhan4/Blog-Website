dfe4d1d4f698021a0ff2cedb19911c52f8d60877
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-10-05 02:33:07 +0530

    Routes added

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Connected to database."))
 .catch((err) => console.log(err));
