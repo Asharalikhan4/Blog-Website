fc867fd931e34e3b71d4a2a348f01177ccc1a4c8
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-10-05 03:25:22 +0530

    All set to connect this with backend

---
+++ Frontend/src/App.js
@@ -0,0 +1,38 @@
+import Topbar from "./components/Topbar/Topbar";
+import Homepage from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Settings from "./pages/Settings/Settings";
+import Single from "./pages/Single/Single";
+import Write from "./pages/Write/Write";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  const currentUser = true;
+  return (
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/posts">
+          <Homepage />
+        </Route>
+        <Route path="/register">
+          {currentUser ? <Homepage /> : <Register />}
+        </Route>
+        <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">
+          {currentUser ? <Settings /> : <Login />}
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
