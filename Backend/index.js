7ca99c8a2a3439b184659788c874f49d194dae60
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-10-05 02:25:43 +0530

    Basic setup done

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Connected to database."))
+.catch((err) => console.log(err));
+
+app.use("/",(req,res)=>{
+    res.send("Hello World!");
+})
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server is up and running on ${process.env.PORT}`);
+})
