6471c2624207b01db4356576835cb970e261f445
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-10-05 03:25:40 +0530

    Backend completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,12 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
@@ -10,9 +16,24 @@ mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Connected to database."))
 .catch((err) => console.log(err));
 
-app.use("/",(req,res)=>{
-    res.send("Hello World!");
-})
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
+  
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+  app.use("/api/posts", postRoute);
+  app.use("/api/categories", categoryRoute);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Server is up and running on ${process.env.PORT}`);
