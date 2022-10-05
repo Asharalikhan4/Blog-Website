5b4e5d1646ac3590791b2b71023fa008ecdf82e1
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-10-05 12:05:21 +0530

    Frontend completed minor changes needed

--- Frontend/src/components/Topbar/Topbar.jsx
+++ Frontend/src/components/Topbar/Topbar.jsx
@@ -48,7 +48,7 @@ export default function TopBar() {
       <div className="topRight">
         {user ? (
           <Link to="/settings">
-            <img className="topImg" src={PF+user.profilePic} alt="" />
+            <img className="topImg" src={PF+user.profilePic} alt="User image" />
           </Link>
         ) : (
           <ul className="topList">
@@ -64,7 +64,6 @@ export default function TopBar() {
             </li>
           </ul>
         )}
-        <i className="topSearchIcon fas fa-search"></i>
       </div>
     </div>
   );
